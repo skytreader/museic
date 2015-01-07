da22eee9e5134a0a9696a48ab41cd2542108d11a
Chad Estioco <chadestioco@gmail.com>
2015-01-08 03:15:20 +0800

    Step-by-step UI prototyping...

--- src/main/java/net/skytreader/kode/museic/ui/MuseicRunnable.java
+++ src/main/java/net/skytreader/kode/museic/ui/MuseicRunnable.java
@@ -7,11 +7,13 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 
 public class MuseicRunnable implements Runnable{
     
     private JFrame mainFrame;
+    private JLabel filePathLabel;
 
     private final int DEFAULT_WIDTH = 500;
     private final int DEFAULT_HEIGHT = 500;
@@ -28,6 +30,15 @@ public class MuseicRunnable implements Runnable{
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         Container mainFrameContainer = mainFrame.getContentPane();
+
+        JPanel filePanel = new JPanel();
+        JButton filePrompt = new JButton("Choose MP3 file");
+        filePathLabel = new JLabel("Currently playing: ");
+        filePanel.add(filePrompt);
+        filePanel.add(filePathLabel);
+        mainFrameContainer.add(filePanel);
+
+        JPanel controlPanel = new JPanel();
 
         mainFrame.setVisible(true);
     }
