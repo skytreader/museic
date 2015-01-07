4ede96fcd62717d7dc8094480ae2cdabcc27bae9
Chad Estioco <chadestioco@gmail.com>
2015-01-08 03:21:39 +0800

    Very initial mock-up.

--- src/main/java/net/skytreader/kode/museic/ui/MuseicRunnable.java
+++ src/main/java/net/skytreader/kode/museic/ui/MuseicRunnable.java
@@ -8,12 +8,14 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.UIManager;
 
 public class MuseicRunnable implements Runnable{
     
     private JFrame mainFrame;
     private JLabel filePathLabel;
+    private JProgressBar seekBar;
 
     private final int DEFAULT_WIDTH = 500;
     private final int DEFAULT_HEIGHT = 500;
@@ -30,6 +32,7 @@ public class MuseicRunnable implements Runnable{
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mainFrame.setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
         Container mainFrameContainer = mainFrame.getContentPane();
+        mainFrameContainer.setLayout(new BoxLayout(mainFrameContainer, BoxLayout.Y_AXIS));
 
         JPanel filePanel = new JPanel();
         JButton filePrompt = new JButton("Choose MP3 file");
@@ -38,7 +41,17 @@ public class MuseicRunnable implements Runnable{
         filePanel.add(filePathLabel);
         mainFrameContainer.add(filePanel);
 
+        seekBar = new JProgressBar();
+        mainFrameContainer.add(seekBar);
+
         JPanel controlPanel = new JPanel();
+        JButton play = new JButton("Play");
+        JButton pause = new JButton("Pause");
+        JButton stop = new JButton("Stop");
+        controlPanel.add(play);
+        controlPanel.add(pause);
+        controlPanel.add(stop);
+        mainFrameContainer.add(controlPanel);
 
         mainFrame.setVisible(true);
     }
